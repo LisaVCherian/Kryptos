34befa5e2345e7065c74ada439f74546529706de
Lisa VC <75480734+LisaVCherian@users.noreply.github.com>
2023-03-01 10:43:58 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   networks: {
     goerli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/VhIXjNgcHTQWi_4iZqVd9UlriPD_8TFM',
-      accounts: ['a01fdc8d8e9b1f6c46e714f824aaf2856b979af821fcd92c6c20bffb094cf991'],
+      accounts: ['fuvkoff'],
     }
   }
 }
